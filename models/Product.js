4f3a3ef58ce0d5d20e7789ecc22204d76d9fc3e1
Miguel Angel <miguelangelteletor@yahoo.es>
2020-05-05 19:10:54 +0200

    basic config ready
    added mongoose config, also added .env basic config, added both Product and User models

---
+++ models/Product.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+       name:String,
+       kind:{
+              type:String,
+              enum:['colchones','somieres']
+       },
+       price:Number
+});
+
+const Product = mongoose.model('Product',ProductSchema);
+module.exports = Product;
