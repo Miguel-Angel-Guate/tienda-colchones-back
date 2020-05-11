94bd9d7b96bdd76f7bd77d8190690ffc64b20864
Miguel Angel <miguelangelteletor@yahoo.es>
2020-05-11 19:53:22 +0200

    delete some files

--- config/keys.js
+++
@@ -1,8 +0,0 @@
- module.exports = {
-     GMAIL: {
-         user: "miguelangelteletor0@gmail.com",
-         pass: "@holahola123"
-     },
-     FRONT_URL: 'http://localhost:4200/',
-     jwt_auth: 'ka894v894s8fe8as89esa48fe94as'
- }
